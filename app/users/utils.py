90aeaec6a7660027d702516b55373f09057664fe
kahenya-anita <anitakahenya1@gmail.com>
2020-09-27 04:06:11 +0300

    Add main routing

--- app/users/utils.py
+++ app/users/utils.py
@@ -7,7 +7,7 @@ from app import mail
 
 
 def save_picture(form_picture):
-    random_hex = secrets.token_hex(6)
+    random_hex = secrets.token_hex(7)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
