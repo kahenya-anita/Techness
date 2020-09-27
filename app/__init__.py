5b86d916ebc268145029dd1f1bd5ffd128466065
kahenya-anita <anitakahenya1@gmail.com>
2020-09-27 13:06:42 +0300

    Add quotes forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,32 @@
 from flask import Flask
-from config import Config
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from app.config import Config, config_options
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
+photos = UploadSet('photos',IMAGES)
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    
+    db.init_app(app)
+    app.config.from_object(config_options[config_name]) 
+    app.config['SECRET_KEY'] = '0806436c2c6ce7'
+    
+    # configure UploadSet
+    configure_uploads(app,photos)
+    
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
--- app/config.py
+++ app/config.py
@@ -1,13 +1,20 @@
 import os
 
 
+
 class Config:
     SECRET_KEY =  os.urandom(32)
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://scgyqlylggnxkt:35e51f5b6aa14afe9bdc51402c729bd461206862a039d4a9d7b6aee2e7ccb4fb@ec2-34-234-185-150.compute-1.amazonaws.com:5432/dfneilmdasjmin'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = '
 
 
 class DevConfig(Config):
