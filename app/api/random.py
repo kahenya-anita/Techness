045a73467ea83a79aecdb1e27d752ee8150fd70e
kahenya-anita <anitakahenya1@gmail.com>
2020-09-28 08:57:24 +0300

    Add quote request by id

--- app/api/random.py
+++ app/api/random.py
@@ -1,8 +1,9 @@
-from app.api import bp
+from flask import jsonify
+from app.models import User
 
 @bp.route('/quotes/<int:id>', methods=['GET'])
 def get_quote(id):
-    pass
+    return jsonify(User.query.get_or_404(id).to_dict())
 
 @bp.route('/quotes', methods=['GET'])
 def get_random():
